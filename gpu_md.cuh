3bbd45e77a518201ce6667f51b0fc76861efa165
reyhaneh <rhn_a_farimani@yahoo.com>
2023-08-01 14:23:32 +0200

    [ISSUE #10] morse potential added.

--- gpu_md.cuh
+++ gpu_md.cuh
@@ -333,16 +333,16 @@ double *L,int size , double ux, int mass, double real_time, int m , int topology
                 }
             }
             
-            // if (ID1==int(m/4) && ID2 ==m+int(3*m/4))
-            // {
+            if (ID1==int(m/4) && ID2 ==m+int(3*m/4))
+            {
                 
-            //     f -= 30/(1 - r_sqr/2.25);
-            // }
+                f -= 2 * 2425.911 * 0.0862* exp(-0.0862*(r_sqr^0.5 + 0.0183)) * (1 - exp(-0.0862*(r_sqr^0.5 + 0.0183)));
+            }
                 
-            // if (ID2==int(m/4) && ID1 ==m+int(3*m/4))
-            // {
-            //     f -= 30/(1 - r_sqr/2.25);
-            // }
+            if (ID2==int(m/4) && ID1 ==m+int(3*m/4))
+            {
+                f -= 2 * 2425.911 * 0.0862* exp(-0.0862*(r_sqr^0.5 + 0.0183)) * (1 - exp(-0.0862*(r_sqr^0.5 + 0.0183)));
+            }
         }
         f/=mass;
 
